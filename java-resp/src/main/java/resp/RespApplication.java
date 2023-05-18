24f6fc9340338363793116908f4f4faef3ae68fe
hanzhang <hanzhang2566@foxmail.com>
2023-05-18 17:27:08 +0800

    feat: 初始化 java-resp 项目

---
+++ java-resp/src/main/java/resp/RespApplication.java
@@ -0,0 +1,19 @@
+package resp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Usage: 启动类 <br/>
+ * Date: 2023/5/18 12:07 <br/>
+ *
+ * @author <a href="mailto:hanzhang2566@foxmail.com">hanzhang</a>
+ */
+@SpringBootApplication
+public class RespApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RespApplication.class, args);
+    }
+
+}
